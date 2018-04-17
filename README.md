738991bf654b528a73d5164bfe88f4ba418af401
Kai Hendry <kai.hendry@unee-t.com>
2018-04-17 17:23:39 +0800

    Background https://github.com/unee-t/frontend/issues/186#issuecomment-381517805

---
+++ README.md
@@ -0,0 +1,5 @@
+lambda: arn:aws:lambda:ap-southeast-1:812644853088:function:alambda_simple
+sns: arn:aws:sns:ap-southeast-1:812644853088:atest
+
+Create a subscription on the SNS topic:
+https://ap-southeast-1.console.aws.amazon.com/sns/v2/home?region=ap-southeast-1#/topics/arn:aws:sns:ap-southeast-1:812644853088:atest
--- functions/simple/main.go
+++ functions/simple/main.go
@@ -6,10 +6,31 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 func handler(ctx context.Context, evt json.RawMessage) (string, error) {
-	return fmt.Sprintf("Ctx: %s\nEvt: %s\n", ctx, evt), nil
+
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return "", err
+	}
+
+	client := sns.New(cfg)
+	req := client.PublishRequest(&sns.PublishInput{
+		Message:  aws.String(fmt.Sprintf("Ctx: %s\nEvt: %s\n", ctx, evt)),
+		TopicArn: aws.String("arn:aws:sns:ap-southeast-1:812644853088:atest"),
+	})
+
+	resp, err := req.Send()
+	if err != nil {
+		return "", err
+	}
+
+	// return fmt.Sprintf("Ctx: %s\nEvt: %s\n", ctx, evt), nil
+	return fmt.Sprintf("Response: %s", resp), nil
 }
 
 func main() {
