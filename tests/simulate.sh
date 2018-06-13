4aa56a049739aee07c10dfae13141bb6ab401e93
Kai Hendry <kai.hendry@unee-t.com>
2018-06-13 16:51:49 +0800

    Would appear SNS is sending JSON payloads in text/plain

--- tests/simulate.sh
+++ tests/simulate.sh
@@ -5,10 +5,10 @@ test -f $1 || exit
 jq -e < $1 || exit
 
 # Test locally
-jq --argfile file $1 '.Message = ($file | tojson)' hook.json | curl -X POST -d @- http://localhost:3000/api/db-change-message/process
+jq --argfile file $1 '.Message = ($file | tojson)' hook.json | curl -H "Content-Type: text/plain" -X POST -d @- http://localhost:3000/api/db-change-message/process
 
 # Use https://sh.unee-t.com/ to verify payload
-#jq --argfile file $1 '.Message = ($file | tojson)' hook.json | curl -X POST -d @- https://sh.unee-t.com/hook
+#jq --argfile file $1 '.Message = ($file | tojson)' hook.json | curl -H "Content-Type: text/plain" -X POST -d @- https://sh.unee-t.com/hook
 
 # Simulate SNS on dev
 #aws --profile uneet-dev sns publish --topic-arn arn:aws:sns:ap-southeast-1:812644853088:atest --message "$(cat $1)"
