d2787e5e061aabd0c970f0a224b0dde2f3e4e79a
Kai Hendry <kai.hendry@unee-t.com>
2019-09-02 11:23:39 +0800

    Clean up

--- tests/call-lambda.sh
+++ tests/call-lambda.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+STAGE=dev
+
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-p]
+
+By default, call the dev environment on AWS account 812644853088
+
+	-p          PRODUCTION 192458993663
+	-d          DEMO 915001051872
+
+EOF
+}
+
+while getopts "pd" opt
+do
+	case $opt in
+		p)
+			echo "PRODUCTION" >&2
+			STAGE=prod
+			;;
+		d)
+			echo "DEMO" >&2
+			STAGE=demo
+			;;
+		*)
+			show_help >&2
+			exit 1
+			;;
+	esac
+done
+AWS_PROFILE=uneet-$STAGE
+shift "$((OPTIND-1))"   # Discard the options and sentinel --
+
+echo Connecting to ${STAGE^^}
+
 if ! test -f "$1"
 then
 	echo Missing JSON payload
@@ -19,25 +55,17 @@ acc() {
 	esac
 }
 
-domain() {
-	case $1 in
-		prod) echo auroradb.unee-t.com
-		;;
-		*) echo auroradb.$1.unee-t.com
-		;;
-	esac
-}
-
-
-for STAGE in dev # prod
-do
-
-echo $STAGE
+echo Calling $STAGE with event $json
 
 ssm() {
-	aws --profile uneet-$STAGE ssm get-parameters --names $1 --with-decryption --query Parameters[0].Value --output text
+	aws --profile $AWS_PROFILE ssm get-parameters --names $1 --with-decryption --query Parameters[0].Value --output text
 }
 
-echo "CALL mysql.lambda_async( 'arn:aws:lambda:ap-southeast-1:$(acc $STAGE):function:alambda_simple', '$(jq -c . $json | phony --max 1)' );" |
-mysql -h $(domain $STAGE) -P 3306 -u $(ssm LAMBDA_INVOKER_USERNAME) --password=$(ssm LAMBDA_INVOKER_PASSWORD)
-done
+echo mysql -h $(ssm MYSQL_HOST) -P 3306 -u $(ssm LAMBDA_INVOKER_USERNAME) --password=$(ssm LAMBDA_INVOKER_PASSWORD)
+if echo "CALL mysql.lambda_async( 'arn:aws:lambda:ap-southeast-1:$(acc $STAGE):function:alambda_simple', '$(jq -c . $json | phony --max 1)' );" |
+mysql -h $(ssm MYSQL_HOST) -P 3306 -u $(ssm LAMBDA_INVOKER_USERNAME) --password=$(ssm LAMBDA_INVOKER_PASSWORD)
+then
+	echo YES
+else
+	echo NO
+fi
